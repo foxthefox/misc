b427dd4bfe9e6de620e79692717624a147c04fc3
foxthefox <16841643+foxthefox@users.noreply.github.com>
2023-01-25 23:10:03 +0100

    Update nodejs-v5.sh

--- nodejs-v5.sh
+++ nodejs-v5.sh
@@ -367,11 +367,11 @@ export PCT_OPTIONS="
   -unprivileged $CT_TYPE
   $PW
 "
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/foxthefox/misc/create_lxc.sh)" || exit
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/foxthefox/misc/main/create_lxc.sh)" || exit
 msg_info "Starting LXC Container"
 pct start $CTID
 msg_ok "Started LXC Container"
-lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/foxthefox/misc/$var_install.sh)" || exit
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/foxthefox/misc/main/$var_install.sh)" || exit
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 pct set $CTID -description "# ${APP} LXC
 msg_ok "Completed Successfully!\n"
